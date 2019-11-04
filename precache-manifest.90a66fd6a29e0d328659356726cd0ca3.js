fc6352e1b702dbec89c8080ac300299851d29237
Chris Braley <chris-braley@hotmail.com>
2019-11-05 07:46:17 +1100

    Updates

---
+++ precache-manifest.90a66fd6a29e0d328659356726cd0ca3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace0e2f81920278dbcf2a315082f625a",
+    "url": "/.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2ad19fb1ea5040a8cb11",
+    "url": "/.github.io/monsters-rolodex/static/css/main.ef7e499f.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/.github.io/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "2ad19fb1ea5040a8cb11",
+    "url": "/.github.io/monsters-rolodex/static/js/main.3c0c4c89.chunk.js"
+  },
+  {
+    "revision": "a70316d1ae67f934c722",
+    "url": "/.github.io/monsters-rolodex/static/js/runtime-main.40731694.js"
+  }
+]);
